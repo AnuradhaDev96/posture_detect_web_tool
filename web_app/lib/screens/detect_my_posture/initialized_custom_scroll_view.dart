954c7fa584f07f42631dc36219bd3dcb9fecde0a
AnuradhaDev96 <anusampath9470@gmail.com>
2023-08-31 11:40:43 +0530

    pause and resume video feature

--- web_app/lib/screens/detect_my_posture/initialized_custom_scroll_view.dart
+++ web_app/lib/screens/detect_my_posture/initialized_custom_scroll_view.dart
@@ -1,11 +1,39 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class InitializedCustomScrollView extends StatelessWidget {
+import '../../themes/app_colors.dart';
+
+class InitializedCustomScrollView extends StatefulWidget {
   InitializedCustomScrollView({super.key, required this.cameraController});
+
   final CameraController cameraController;
 
+  @override
+  State<InitializedCustomScrollView> createState() => _InitializedCustomScrollViewState();
+}
+
+class _InitializedCustomScrollViewState extends State<InitializedCustomScrollView> {
   final _scrollController = ScrollController();
+  bool _isPreviewPaused = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _pauseCamera() async {
+    setState(() {
+      widget.cameraController.pausePreview();
+      _isPreviewPaused = true;
+    });
+  }
+
+  void _resumeCamera() async {
+    setState(() {
+      widget.cameraController.resumePreview();
+      _isPreviewPaused = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +51,62 @@ class InitializedCustomScrollView extends StatelessWidget {
                 borderRadius: BorderRadius.circular(16),
               ),
               child: Center(
-                child: AspectRatio(
-                  aspectRatio: cameraController.value.aspectRatio,
-                  child: CameraPreview(cameraController),
-                ),
+                child: _isPreviewPaused
+                    ? GestureDetector(
+                        onTap: () => _resumeCamera(),
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                            width: 200,
+                            height: 50,
+                            margin: const EdgeInsets.only(top: 12),
+                            decoration: BoxDecoration(
+                              color: AppColors.indigo1,
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                'Resume',
+                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : AspectRatio(
+                        aspectRatio: widget.cameraController.value.aspectRatio,
+                        child: CameraPreview(widget.cameraController),
+                      ),
               ),
             ),
           ),
         ),
+
+        // show take a break button when camera preview is not paused
+        if (!_isPreviewPaused)
+          SliverToBoxAdapter(
+            child: GestureDetector(
+              onTap: () => _pauseCamera(),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  width: 200,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 12),
+                  decoration: BoxDecoration(
+                    color: AppColors.indigo1,
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Take a Break',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
       ],
     );
   }
